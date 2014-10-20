623e942fce21fe431509a651d73915e30a434fdc
Simon Kusterer <simon@soped.com>
2014-10-20 19:50:49 +0200

    deleted some unused files

--- examples/attach.js
+++
@@ -1,7 +0,0 @@
-var player = require('../')();
-
-player.attach(function(err, p) {
-  if (err) return console.log(err);
-  console.log('attached to playback session');
-  p.pause();
-});
--- examples/start.js
+++
@@ -1,7 +0,0 @@
-var player = require('../')();
-
-player.launch('http://commondatastorage.googleapis.com/gtv-videos-bucket/big_buck_bunny_1080p.mp4', function(err, p) {
-  p.once('playing', function() {
-    console.log('playback started');
-  });
-});
